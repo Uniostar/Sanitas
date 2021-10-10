8a8fc46ddb40a760b132ae073d3c4bf4efc4e018
Uniton2 <akvkmg2007@gmail.com>
2021-10-10 06:35:32 +0530

    Rep #

--- Dominators Project/Assets/Scripts/Logic.cs
+++ Dominators Project/Assets/Scripts/Logic.cs
@@ -5,18 +5,25 @@ using UnityEngine.UI;
 
 public class Logic : MonoBehaviour
 {
+    //Text Outputs
     public Text sleepText, heartDataText, heartRateRatingText, spo2Text, bmiValueText, bmiPropertyText, bmiRateText, healthRating;
+
+    //Inputs
     public InputField ageInput, pulseInput, spo2Input, heightInput, weightInput, sleepInput;
+
+    //Checkmarks
     public Toggle heart, kidney, asthama, sleepDisorders;
 
+    //Scenes
     public GameObject scene1;
     public GameObject scene2;
     public GameObject scene3;
 
-    string[] info;
+    //Identifiers needed for calculating health rating
     float age, heartRate, spo2, height, weight, sleep, BMI;
     float heartRateRating, SPO2Rating, BMIRate, sleep_time_Rating;
 
+    //Set Up Scene  1
     void Start()
     {
         scene1.SetActive(true);
@@ -52,14 +59,19 @@ public class Logic : MonoBehaviour
         System.out.println("Your Sleep and BMI determine your health in your day-today life. Despite not being as important as the heart rate and Oxygen saturation, they must still be well maintained, as in the long term this can lead to health problems");
         */
     }
+
+    //Switch to Scene 2
     public void GoToScene2()
     {
         scene1.SetActive(false);
         scene2.SetActive(true);
         scene3.SetActive(false);
     }
+
+    //Switch to Scene 3
     public void GoToScene3()
     {
+        //Convert Input String to Float
         age = float.Parse(ageInput.text);
         heartRate = float.Parse(pulseInput.text);
         spo2 = float.Parse(spo2Input.text);
@@ -67,123 +79,142 @@ public class Logic : MonoBehaviour
         weight = float.Parse(weightInput.text);
         sleep = float.Parse(sleepInput.text);
 
+        //Open Scene 3
         scene1.SetActive(false);
         scene2.SetActive(false);
         scene3.SetActive(true);
 
+        //If Sleep Disorders is Checked
         if (sleepDisorders.isOn)
         {
+            //If sleep time is greater than 9 hours
             if (sleep > 9)
             {
-                sleepText.text = "Please visit a sleep specialist, because you might have hypersomia!";
+                sleepText.text = "> Please visit a sleep specialist, because you might have hypersomia!";
                 sleep_time_Rating = 10;
             }
+            //If sleep time is between 9 and 7 hours
             if ((sleep < 9) && (sleep > 7))
             {
-                sleepText.text = "You have a healthy sleep schedule!";
+                sleepText.text = "> You have a healthy sleep schedule!";
                 sleep_time_Rating = 10;
             }
+            //If sleep time is under 7 hours
             if (sleep < 7)
             {
-                sleepText.text = "You need to work on your sleep schedule";
+                sleepText.text = "> You need to work on your sleep schedule";
                 sleep_time_Rating = (8 - sleep) * 125 / 100;
             }
         }
+        //If Sleep Disorders is Unchecked
         else
         {
+            //If sleep time is greater than 9 hours
             if (sleep > 9)
             {
-                sleepText.text = ("Your sleep shoes no problems!");
+                sleepText.text = ("> Your sleep shoes no problems!");
                 sleep_time_Rating = 10;
             }
+            //If sleep time is between 9 and 7 hours
             if ((sleep < 9) && (sleep > 7))
             {
-                sleepText.text = "You have a healthy sleep schedule!";
+                sleepText.text = "> You have a healthy sleep schedule!";
                 sleep_time_Rating = 10;
             }
+            //If sleep time is under 7 hours
             if (sleep < 7)
             {
-                sleepText.text = "You need to work on your sleep schedule";
+                sleepText.text = "> You need to work on your sleep schedule";
                 sleep_time_Rating = (8 - sleep) * 125 / 100;
             }
 
         }
+        //If Age is greater than 18
         if (age > 18)
         {
+            //If heart rate is greater than 90
             if (heartRate > 90)
             {
-                heartDataText.text = ("Heart rate too high. Consult a doctor. Do deep breaths, exercise well, and maintain a healthy diet reducing coffee consumption. Don't panic if you are stressed, everything will be fine.");
+                heartDataText.text = ("> Heart rate too high. Consult a doctor. Do deep breaths, exercise well, and maintain a healthy diet reducing coffee consumption. Don't panic if you are stressed, everything will be fine.");
                 float heartRateDev = (heartRate - 110) / 10;
                 heartRateRating = 8 - heartRateDev;
             }
+            //If Heart rate is less than 60
             else if (heartRate < 60)
             {
-                heartDataText.text = ("Heart rate too low. Consult a doctor");
+                heartDataText.text = ("> Heart rate too low. Consult a doctor");
                 float heartRateDev = (60 - heartRate) / 10;
                 heartRateRating = 8 - heartRateDev;
             }
+            //If heart rate is between 90 and 60
             else
             {
-                heartDataText.text = ("Heart rate is healthy");
+                heartDataText.text = ("> Heart rate is healthy");
                 float heartRateDev = (Mathf.Abs(heartRate - 85)) / 10;
                 heartRateRating = 10 - heartRateDev;
             }
         }
+        //If age is under or equal to 18
         else
         {
+            //If heart rate is greater than 120
             if (heartRate > 120)
             {
-                heartDataText.text = ("Heart rate too high. Consult a doctor. Do deep breaths and maintain a healthy diet. Don't panic if you are stressed, everything will be fine.");
+                heartDataText.text = ("> Heart rate too high. Consult a doctor. Do deep breaths and maintain a healthy diet. Don't panic if you are stressed, everything will be fine.");
                 float heartRateDev = (heartRate - 110) / 10;
                 heartRateRating = 8 - heartRateDev;
             }
+            //If heart rate is less than 50
             else if (heartRate < 50)
             {
-                heartDataText.text = ("Heart rate too low. Consult a doctor.");
+                heartDataText.text = ("> Heart rate too low. Consult a doctor.");
                 float heartRateDev = (50 - heartRate) / 10;
                 heartRateRating = 8 - heartRateDev;
             }
+            //If heart rate is between 120 and 50
             else
             {
-                heartDataText.text = ("Heart rate is healthy");
+                heartDataText.text = ("> Heart rate is healthy");
                 float heartRateDev = (Mathf.Abs(heartRate - 85)) / 10;
                 heartRateRating = 10 - heartRateDev;
             }
         }
 
-        heartRateRatingText.text = ("Heart Rate Rating : " + heartRateRating.ToString());
+        //Calculate Heart Rate Rating and Print It
+        heartRateRatingText.text = ("> Heart Rate Rating : " + heartRateRating.ToString());
 
+        //Calculate SpO2 Rating and Print It
         SPO2Rating = (spo2 - 80) / 2;
 
         if (spo2 <= 94)
         {
-            spo2Text.text = ("Your oxygen level is low. Consult a doctor. " + SPO2Rating + " is your SpO2 Rating.");
+            spo2Text.text = ("> Your oxygen level is low. Consult a doctor. " + SPO2Rating + " is your SpO2 Rating.");
         }
         else
         {
-            spo2Text.text = ("Your oxygen levels are healthy " + SPO2Rating + " is your SpO2 Rating.");
+            spo2Text.text = ("> Your oxygen levels are healthy " + SPO2Rating + " is your SpO2 Rating.");
         }
 
         BMI = weight / (Mathf.Pow((height / 100), 2));
-        bmiValueText.text = ("Your BMI is " + BMI + " kg/m²");
+        bmiValueText.text = ("> Your BMI is " + BMI + " kg/m²");
 
         if (age > 20)
         {
             if (BMI >= 25.0)
             {
-                bmiPropertyText.text = ("Your BMI is higher than recommended levels");
+                bmiPropertyText.text = ("> Your BMI is higher than recommended levels");
                 float BMIDev = BMI - 25;
                 BMIRate = 8 - BMIDev;
             }
             if ((BMI < 24.9) && (BMI > 18.5))
             {
-                bmiPropertyText.text = ("You have a healthy BMI");
+                bmiPropertyText.text = ("> You have a healthy BMI");
                 float BMIDev = Mathf.Abs(BMI - 21.7f);
                 BMIRate = 10 - BMIDev;
             }
             if (BMI < 18.5)
             {
-                bmiPropertyText.text = ("Your BMI is lower than recommended levels");
+                bmiPropertyText.text = ("> Your BMI is lower than recommended levels");
                 float BMIDev = 18.5f - BMI;
                 BMIRate = 8 - BMIDev;
             }
@@ -194,19 +225,19 @@ public class Logic : MonoBehaviour
             {
                 if (BMI < 14)
                 {
-                    bmiPropertyText.text = ("Your BMI is lower than recommended levels");
+                    bmiPropertyText.text = ("> Your BMI is lower than recommended levels");
                     float BMIDev = 14 - BMI;
                     BMIRate = 8 - BMIDev;
                 }
                 else if (BMI > 18)
                 {
-                    bmiPropertyText.text = ("Your BMI is higher than recommended levels");
+                    bmiPropertyText.text = ("> Your BMI is higher than recommended levels");
                     float BMIDev = BMI - 18;
                     BMIRate = 8 - BMIDev;
                 }
                 else
                 {
-                    bmiPropertyText.text = ("You have a healthy BMI");
+                    bmiPropertyText.text = ("> You have a healthy BMI");
                     float BMIDev = Mathf.Abs(BMI - 16);
                     BMIRate = 10 - BMIDev;
                 }
@@ -215,19 +246,19 @@ public class Logic : MonoBehaviour
             {
                 if (BMI < 14.5)
                 {
-                    bmiPropertyText.text = ("Your BMI is lower than recommended levels");
+                    bmiPropertyText.text = ("> Your BMI is lower than recommended levels");
                     float BMIDev = 14.5f - BMI;
                     BMIRate = 8 - BMIDev;
                 }
                 else if (BMI > 22)
                 {
-                    bmiPropertyText.text = ("Your BMI is higher than recommended levels");
+                    bmiPropertyText.text = ("> Your BMI is higher than recommended levels");
                     float BMIDev = BMI - 22;
                     BMIRate = 8 - BMIDev;
                 }
                 else
                 {
-                    bmiPropertyText.text = ("You have a healthy BMI");
+                    bmiPropertyText.text = ("> You have a healthy BMI");
                     float BMIDev = Mathf.Abs(BMI - 18.25f);
                     BMIRate = 10 - BMIDev;
                 }
@@ -236,26 +267,26 @@ public class Logic : MonoBehaviour
             {
                 if (BMI < 17)
                 {
-                    bmiPropertyText.text = ("Your BMI is lower than recommended levels");
+                    bmiPropertyText.text = ("> Your BMI is lower than recommended levels");
                     float BMIDev = 17 - BMI;
                     BMIRate = 8 - BMIDev;
                 }
                 else if (BMI > 26)
                 {
-                    bmiPropertyText.text = ("Your BMI is higher than recommended levels");
+                    bmiPropertyText.text = ("> Your BMI is higher than recommended levels");
                     float BMIDev = BMI - 26;
                     BMIRate = 8 - BMIDev;
                 }
                 else
                 {
-                    bmiPropertyText.text = ("You have a healthy BMI");
+                    bmiPropertyText.text = ("> You have a healthy BMI");
                     float BMIDev = Mathf.Abs(BMI - 21.5f);
                     BMIRate = 8 - BMIDev;
                 }
             }
         }
-        bmiRateText.text = ("Your BMI Rate is - " + BMIRate.ToString());
-        healthRating.text = "Health Rate Index : " + (((SPO2Rating) + (heartRateRating) + (BMIRate) + (sleep_time_Rating)) / 4).ToString();
+        bmiRateText.text = ("> Your BMI Rate is - " + BMIRate.ToString());
+        healthRating.text = "> Health Rate Index : " + (((SPO2Rating) + (heartRateRating) + (BMIRate) + (sleep_time_Rating)) / 4).ToString();
     }
     public void TestEyeSight()
     {
